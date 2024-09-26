cf1e11a63c14999290265309c7d0d98380ea82e8
Vamshi <Vamshiteja.vt@gmail.com>
2024-09-26 17:05:04 +0530

    new lamba4

---
+++ lambda4/index.js
@@ -0,0 +1,9 @@
+export const handler = async (event) => {
+  // TODO implement
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('Cool it is working!'),
+  };
+  return response;
+};
+
