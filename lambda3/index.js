ab2c0fef4f6a8fb3660621341dc05d9ca25c0e3d
Vamshi <Vamshiteja.vt@gmail.com>
2024-09-26 16:03:42 +0530

    modified lambda and workflow

--- lambda3/index.js
+++ lambda3/index.js
@@ -4,7 +4,7 @@ exports.handler = async (event) => {
     return {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'Lambda function is working!',
+            message: 'Cool it is working!',
         }),
     };
 };
